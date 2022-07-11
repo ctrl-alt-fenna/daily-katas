c52bb8a41d8fd21d4b41873db8746abf0d037819
Fenna <ikbenfenna@gmail.com>
2022-07-11 08:06:55 +0200

    ADD: Extra kata

--- extra/true/index.test.ts
+++ extra/true/index.test.ts
@@ -1,12 +1,4 @@
 import { countTrue } from ".";
-/*
-countTrue([true, false, false, true, false]) ➞ 2
-
-countTrue([false, false, false, false]) ➞ 0
-
-countTrue([]) ➞ 0
-
-*/
 test ("countTrue([true, false, false, true, false]) should equal 2", () => {
     expect(countTrue([true, false, false, true, false])).toBe(2);
 });
