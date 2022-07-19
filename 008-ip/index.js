30349c3b6f3bc47434681223e7d79f76bac98fe5
Fenna <ikbenfenna@gmail.com>
2022-07-19 09:28:04 +0200

    MOD: Removed equal sign for 255 in kata 8

--- 008-ip/index.js
+++ 008-ip/index.js
@@ -4,7 +4,7 @@ export function isValidIP(ipstr)
     const splitIP = ipstr.split('.')
     if (splitIP.length !== 4) return false
     for (const num of splitIP) {
-        if (num[0] !== '0' && (num >= 1 && num <= 255)) ans = true
+        if (num[0] !== '0' && (num >= 1 && num < 255)) ans = true
         else return false
     }
     return ans
