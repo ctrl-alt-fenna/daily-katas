f5f42ee3d2a99ee7b464771ed25d5141db92525e
Fenna <ikbenfenna@gmail.com>
2022-07-19 09:36:55 +0200

    MOD: Fixed small error in Kata 8

--- 008-ip/index.js
+++ 008-ip/index.js
@@ -1,11 +1,9 @@
 export function isValidIP(ipstr)
 {
-    let ans = false 
     const splitIP = ipstr.split('.')
-    if (splitIP.length !== 4) return false
+    if (splitIP.length !== 4 || ipstr.endsWith('0')) return false
     for (const num of splitIP) {
-        if (num[0] !== '0' && (num >= 1 && num < 255)) ans = true
-        else return false
+        if (num[0] === '0' || (num < 1 || num > 255)) return false;
     }
-    return ans
+    return true
 }
